f933205ffb5e717aa3d0f716c13a2b6db52abfc9
MrsSpindler <karen.spindler@ocsb.ca>
2018-03-06 10:36:46 -0500

    Initial commit

---
+++ ECOO2018_Java/src/ECOO/ECOO2018P1.java
@@ -0,0 +1,27 @@
+package ECOO;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class ECOO2018P1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader fileIn = new BufferedReader(new FileReader("res/DATA10.txt"));
+
+		//expect 10 runs of the program (not necessarily 10 lines!) in the file
+		for (int k = 0; k < 10; k++) {
+
+			int integerInFile = Integer.parseInt(fileIn.readLine());
+			double doubleInFile = Double.parseDouble(fileIn.readLine());
+			String stringInFile = fileIn.readLine();
+			String [] listOfStringsInFile = fileIn.readLine().split(" ");
+			
+			System.out.println("Read " + (k+1) + ": " + integerInFile + " " + doubleInFile + " " + stringInFile + " " + Arrays.deepToString(listOfStringsInFile));
+			
+		}
+
+		fileIn.close(); // close the file.
+	}
+
+}
---
+++ ECOO2018_Python/ECOO2018P1.py
@@ -0,0 +1,31 @@
+'''
+Sample File Reading program for ECOO contests
+'''
+fileIn = open("res/DATA10.txt", 'r')
+
+#expect 10 test cases; read the problem to understand how they should be formatted
+for k in range (0, 10):
+    
+    print("Reading Test case #",k)
+    
+    #demonstrating how to read different types of input from a file
+    
+    #expecting a single integer?  Use this:
+    expectedInteger = int(fileIn.readline())
+    print(expectedInteger)
+    
+    #expecting a float? Use this:
+    expectedFloat = float(fileIn.readline())
+    print(expectedFloat)
+    
+    #expecting a String? Use this to avoid have the newline character included on the end of the string
+    expectedString = fileIn.readline().strip('\n')
+    print(expectedString)
+    
+    #expecting a list of items?  Use this.  Change into a number if needed
+    expectedList = fileIn.readline().split()
+    
+    for item in expectedList:
+        print(item)
+
+fileIn.close()
